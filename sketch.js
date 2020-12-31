40cc9186bb74803db3dc3d7d936e757e47364c68
Hrithik2008 <64474232+Hrithik2008@users.noreply.github.com>
2020-12-31 23:13:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 
-var engine,world,ground,gift,giftSprite,giftImg,mc,mcImg,message,strl;
+var engine,world,ground,gift,giftSprite,giftImg,mc,mcImg,message;
 
 function preload(){
   giftImg=loadImage("GImg.jpg");
@@ -39,9 +39,6 @@ giftSprite.scale=0.3;
   mc.addImage(mcImg);
   mc.visible=false;
   mc.scale=1.6;
-
-  message="Best Wishes from Sudhakar and family";
-  strl=textWidth(message);
 }
 
 function draw() {
@@ -69,7 +66,6 @@ function draw() {
 fill("white");
     textSize(40);
     text("Developed by Hrithik Ram Sudhakaran",0,windowHeight-10);
-    text(message,windowWidth-strl*1.45,windowHeight-10);
     if(mc.y<windowHeight-(windowHeight/2)){
       mc.velocityY=0;
     }
